8488b5a8beac50f49e5d998ab7d418eea40ba5ba
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-05 11:07:04 +0530

    Create integer.java

---
+++ integer.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.lang.*;
+inport java.io.*;
+public class integer
+{
+public static void main(String[] args)
+{
+char ch,result i;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the char");
+ch=sc.nextLine();
+int len=str.length();
+    for(int i=0;i<len;i++)
+     char ch=str.charAt(i);
+  if(ch='I')
+  {
+    result i=i++;
+  }
+  if(ch="II")
+  {
+  result i=i++;
+  }
+  if(ch="III")
+  {result i=i++;
+  }
+  if(ch="IV")
+  {
+  result i=i++;
+  }
+  if(ch="V")
+  {
+  result i=i++;
+  }
+  if(ch="VI")
+  {
+  result i=i++;
+  }
+  if(ch="VII")
+{
+result i=i++;
+}
+if(ch="VIII")
+{result i=i++;
+}
+if(ch="XI")
+{
+result i=i++;
+}
+if(ch="x")
+{
+result i=i++;
+}
+  System.out.println("integer"+result i);
+  }
+  }
